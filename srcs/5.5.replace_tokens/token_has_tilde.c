4a5241e7305e5944b078006e086044c84480da5a
Diogo Pestana vilarinho <dpestana@c2r9s4.42lisboa.com>
2022-10-17 14:50:49 +0100

    42

--- srcs/5.5.replace_tokens/token_has_tilde.c
+++ srcs/5.5.replace_tokens/token_has_tilde.c
@@ -6,7 +6,7 @@
 /*   By: dpestana <dpestana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 12:21:33 by dpestana          #+#    #+#             */
-/*   Updated: 2022/10/17 14:10:54 by dpestana         ###   ########.fr       */
+/*   Updated: 2022/10/17 14:29:02 by dpestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int token_has_tilde(t_data *data, int inc)
 {
     if (*(*(data->tmp.cmd->token + inc)) == '~')
         return (YES);
-    if (*(*(data->tmp.cmd->token + inc)) == '~' && *(*(data->tmp.cmd->token + inc)) == '/')
+    if (*(*(data->tmp.cmd->token + inc)) == '~' && *(*(data->tmp.cmd->token + inc) + 1) == '/')
         return (YES);
     return (NO);
 }
