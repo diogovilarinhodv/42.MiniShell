43392846b1f980c1b3e12ed939ead3af3e14e08b
digui13 <digui13>
2023-02-14 11:15:28 +0000

    42

--- srcs/5.input_handler/2.token_creation_get/get_quote_pos_end.c
+++ srcs/5.input_handler/2.token_creation_get/get_quote_pos_end.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_quote_pos_end.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpestana <dpestana@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpestana <dpestana@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 13:25:40 by dpestana          #+#    #+#             */
-/*   Updated: 2023/02/06 18:35:31 by dpestana         ###   ########.fr       */
+/*   Updated: 2023/02/14 11:15:00 by dpestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,31 @@
 
 void	get_quote_pos_end(t_data *data)
 {
+	int	len;
+
+	len = 0;
 	if (*(data->input.line + data->input.idx) == '"')
 	{
 		data->input.idx++;
-		while (*(data->input.line + data->input.idx) != '"')
-			data->input.idx++;
-		data->input.idx++;
+		len = ft_strlen(data->input.line);
+		while (data->input.idx < len)
+		{
+			if (*(data->input.line + len) == '"')
+				break ;
+			len--;
+		}
+		data->input.idx = len + 1;
 	}
 	else if (*(data->input.line + data->input.idx) == '\'')
 	{
 		data->input.idx++;
-		while (*(data->input.line + data->input.idx) != '\'')
-			data->input.idx++;
+		len = ft_strlen(data->input.line);
+		while (data->input.idx < len)
+		{
+			if (*(data->input.line + len) == '\'')
+				break ;
+			len--;
+		}
 		data->input.idx++;
 	}
 }
