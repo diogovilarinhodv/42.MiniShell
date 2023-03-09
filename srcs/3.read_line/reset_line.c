e1712aaab83505acbd94eb8bdf1ef926761ae004
digui13 <digui13>
2023-03-09 19:15:02 +0000

    42

--- srcs/3.read_line/reset_line.c
+++ srcs/3.read_line/reset_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   reset_line.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpestana <dpestana@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpestana <dpestana@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 22:58:56 by dpestana          #+#    #+#             */
-/*   Updated: 2023/03/05 15:24:53 by dpestana         ###   ########.fr       */
+/*   Updated: 2023/03/09 00:37:20 by dpestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	reset_line(t_data *data)
 {
 	write_str("^C\n");
 	data->exit_status = EXIT_CMD_INTERRUPTED;
+	set_exit_status_env(data);
 	write_prompt(data);
 	ft_bzero(data->input.buf, BUFSIZ);
 	data->input.buf_idx = 0;
--- srcs/8.execute/1.heredoc/heredoc_reset_line.c
+++ srcs/8.execute/1.heredoc/heredoc_reset_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc_reset_line.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpestana <dpestana@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpestana <dpestana@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 15:25:08 by dpestana          #+#    #+#             */
-/*   Updated: 2023/03/06 16:14:34 by dpestana         ###   ########.fr       */
+/*   Updated: 2023/03/09 00:46:05 by dpestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	heredoc_reset_line(t_data *data)
 {
 	write_str("^C\n");
 	data->exit_status = EXIT_CMD_INTERRUPTED;
+	set_exit_status_env(data);
 	ft_bzero(data->input.buf, BUFSIZ);
 	data->input.buf_idx = 0;
 }
