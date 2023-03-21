f65f393f36e482df1b8a057159e6afe2525e0507
Diogo Pestana vilarinho <dpestana@c2r5s4.42lisboa.com>
2023-03-21 17:43:48 +0000

    42

--- srcs/11.builtins/2.builtin_echo/echo_has_new_line.c
+++ srcs/11.builtins/2.builtin_echo/echo_has_new_line.c
@@ -6,7 +6,7 @@
 /*   By: dpestana <dpestana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 16:45:18 by dpestana          #+#    #+#             */
-/*   Updated: 2023/01/19 16:46:31 by dpestana         ###   ########.fr       */
+/*   Updated: 2023/03/21 17:40:51 by dpestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 
 int	echo_has_new_line(t_data *data, int *y)
 {
+	int	inc;
+
+	inc = 1;
 	if (data->cur.cmd->qty_tkn > 1)
 	{
-		if (ft_strcmp(*(data->cur.cmd->token + 1), "-n") == 0)
+		if (**(data->cur.cmd->token + 1) == '-'
+			&& *(*(data->cur.cmd->token + 1) + inc) == 'n')
 		{
-			(*y)++;
-			return (NO);
+			while (*(*(data->cur.cmd->token + 1) + inc) == 'n')
+				inc++;
+			if (*(*(data->cur.cmd->token + 1) + inc) == '\0')
+			{
+				(*y)++;
+				return (NO);
+			}
 		}
 	}
 	return (YES);
