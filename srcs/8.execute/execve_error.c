1b1c60968673dc52d63f1cd492c451751a28ec3c
Diogo Pestana vilarinho <dpestana@c2r10s6.42lisboa.com>
2023-03-24 15:09:06 +0000

    42

--- srcs/8.execute/execve_error.c
+++ srcs/8.execute/execve_error.c
@@ -6,11 +6,23 @@
 /*   By: dpestana <dpestana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 17:35:04 by dpestana          #+#    #+#             */
-/*   Updated: 2023/03/24 14:28:00 by dpestana         ###   ########.fr       */
+/*   Updated: 2023/03/24 15:07:05 by dpestana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/minishell.h"
+
+static void	write_error(t_data *data, char *msg, char *str, int n)
+{
+	write_str(str);
+	write_str(msg);
+	write_str("\n");
+	unset_env_var_full(data);
+	close(data->stdin_saved);
+	close(data->stdout_saved);
+	close_all_fd(data);
+	end_program(data, n);
+}
 
 static void	exec_enoent_error(t_data *data)
 {
@@ -59,25 +71,13 @@ static void	exec_eacces_error(t_data *data, char *str)
 		write_error(data, ": Permission denied", str, EXIT_CMD_INTERRUPTED);
 }
 
-static void	write_error(t_data *data, char *msg, char *str, int n)
-{
-	write_str(str);
-	write_str(msg);
-	write_str("\n");
-	unset_env_var_full(data);
-	close(data->stdin_saved);
-	close(data->stdout_saved);
-	close_all_fd(data);
-	end_program(data, n);
-}
-
 void	execve_error(t_data *data)
 {
 	char		*str;
 
 	str = *data->cur.cmd->token;
 	if (errno == EACCES)
-		exec_eacces_error(data);
+		exec_eacces_error(data, str);
 	else if (errno == ENOENT)
 		exec_enoent_error(data);
 	unset_env_var_full(data);
